a44df0652247f58ca63ed522ffd8c90f3a65ad7d
Jason M Brady <jasonmbrady42@gmail.com>
2021-12-09 18:25:38 -0800

    wk2 day2

---
+++ week2/JSDemo/script.js
@@ -0,0 +1,15 @@
+
+// function swapSpan(){
+//     document.querySelector("#blueText").innerText = "Javascript!";
+// }
+ function swapSpan(element){
+    element.style.color = "#aa00aa"
+ }
+ function like(id){
+     document.querySelector("#" + id).innerText++;
+ }
+ var counter = 0;
+ function clickAlert(){
+     counter++;
+     alert("You clicked me " + counter + " times!")
+ }
